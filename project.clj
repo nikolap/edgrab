36605e45fa3ba52bf5a800a2bc0cf3c9619b9133
Nikola Peric <nikola.peric03@gmail.com>
2014-03-02 16:50:37 -0500

    Minor change

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject edgrab "1.0.0"
+(defproject edgrab "1.0.1"
   :description "Short script to grab XBRL files from EDGAR"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/nikolap/edgrab"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje "1.6.2"]]
-  :main edgrab.core)
+                 [midje "1.6.2"]])
